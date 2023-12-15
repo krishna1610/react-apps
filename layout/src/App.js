9a9bde41e46503c341ccef03b322abba24108251
Krishna Bhadkoliya <kkbhadkoliya16@gmail.com>
2023-12-15 11:17:03 -0800

    layout app

---
+++ layout/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+      <header>Header</header>
+      <div className='middle-section'>
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
